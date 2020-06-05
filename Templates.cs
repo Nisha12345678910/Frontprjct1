4d03ae7cf14b3f62b5aa038188937904131ef3ca
Nisha12345678910 <40429940+Nisha12345678910@users.noreply.github.com>
2020-06-05 08:42:57 -0700

    Add files via upload

---
+++ Templates.cs
@@ -0,0 +1,96 @@
+﻿using Sitecore.Data;
+
+namespace Utilities
+{
+    public static class Templates
+    {
+
+        public static class Logo
+        {
+            public static class Fields
+            {
+                public static readonly ID LogoImage = new ID("{CC5F6B71-48C5-406D-A6D0-BDF7DB55ACAC}");
+                public static readonly ID LogoLink = new ID("{FBB0C2EC-9282-46E0-B6B4-E524FEBA4230}");
+             }
+        }
+        public static class Banner
+        {
+            public static class Fields
+            {
+                public static readonly ID BannnerText = new ID("{6CFA8B0C-47F7-48BB-A8A8-5352ABCF4ACE}");
+                public static readonly ID BannerCaption = new ID("{A038DF7F-680A-42C0-A23D-17C1FFDA0595}");
+                public static readonly ID BannerImage = new ID("{F5137A22-4F69-4F3C-82B3-DED34F1A589C}");
+            }
+        }
+        public static class Callout
+        {
+            public static class Fields
+            {
+                public static readonly ID HomeCalloutSubtitle= new ID("{DA5DC49B-0A19-4AE9-92DB-CA3468217AA3}");
+                public static readonly ID HomeCalloutLink = new ID("{EB1CD7AF-C546-48DA-9ABB-973060374FEE}");
+                public static readonly ID HomeCalloutTitle = new ID("{972FC750-E085-4E53-B303-FEDAF5AB55AB}");
+                public static readonly ID HomeCalloutImage = new ID("{107C3386-E9B7-4738-BDDE-AB5700508472}");
+                 
+            }
+        }
+        public static class SectionCallout
+        {
+            public static class Fields
+            {
+                public static readonly ID SectionSubtitle = new ID("{B96AD882-F551-479C-80E1-E8D214370581}");
+                public static readonly ID SectionLink = new ID("{CBC2DE0A-D2D8-466E-A50F-6A7194B74B2B}");
+                public static readonly ID SectionTitle = new ID("{542436A4-6E78-497E-ACC3-29F58E454990}");
+                public static readonly ID SectiontImage = new ID("{FC30B4C8-BFFD-4262-B04F-3D0C46AECA29}");
+
+            }
+        }
+        public static class ProductBanner
+        {
+            public static class Fields
+            {
+                public static readonly ID ProductBannnerText = new ID("{E42DBAC8-C614-4103-A43B-F8391E963609}");
+                public static readonly ID ProductBannerCaption = new ID("{B40961AA-7F57-4E60-88F8-E9A80DE91D67}");
+                public static readonly ID ProductBannerImage = new ID("{247602E3-4B57-428F-8E3F-E1928BE04241}");
+
+            }
+        }
+        public static class ProductCallout
+        {
+            public static class Fields
+            {
+                public static readonly ID ProductCalloutTitle = new ID("{79073822-3076-44EC-9E10-BDF48052F770}");
+                public static readonly ID ProductCalloutSubTutle = new ID("{F4E44FF7-E5AE-4E4D-A7EA-E1FCFB1A9E92}");
+                public static readonly ID ProductCalloutImage = new ID("{7B56ED74-1177-4343-AA17-BA47E0C8F617}");
+                public static readonly ID ProductCalloutLink = new ID("{866F694D-341A-43FE-A18C-47C29D4486D8}");
+
+            }
+        }
+        public static class ProductDetails
+        {
+            public static class ProductDetailTopFields
+            {
+                public static readonly ID ProductDetailsTitle = new ID("{6D323FD9-EDBA-4320-8EE3-58E5BD965761}");
+                public static readonly ID ProductSubTitle = new ID("{B7903901-0BBC-458B-8607-BE9E2428E711}");
+                public static readonly ID ProductDetailsImage = new ID("{BECFC577-94AF-4F5E-9D1D-50E67AE76B95}");
+                public static readonly ID ProductPrice = new ID("{FE20515B-2B9E-4662-97FF-2DEBFBEE1C06}");
+                public static readonly ID ProductPriceValue = new ID("{4D456017-38AC-4E23-82E0-41D7FACF296F}");
+                public static readonly ID ProductDescription = new ID("{7C0ABD6F-519B-4AE0-B39C-CCAA555C97B5}");
+
+
+            }
+            public static class RelatedProductCallout
+            {              
+                public static readonly ID RelatedProductTitle = new ID("{1F8AEF2A-16D0-4677-9090-AD4E6BEFC768}");
+                //Product Callout
+             }
+        }
+        public static class PageItem
+        {
+            public static class Fields
+            {
+                public static readonly ID TempId = new ID("{A87A00B1-E6DB-45AB-8B54-636FEC3B5523}");
+                
+            }
+        }
+    }
+}
